6b894e46fb6ea0a7f4c4c928a0694ee21255461d
Sunloid <syedrazvi.dev@gmail.com>
2026-09-25 10:06:24 +0530

    Commit

---
+++ modules/s3-origin/variables.tf
@@ -0,0 +1,16 @@
+variable "aws_region" {
+    description = "AWS region to deploy into" 
+    type = string
+    default = "ap-south-1"
+}
+
+variable "project_name" {
+    description = "Name used to prefix/tag all resources" 
+    type = string
+    default = "cloudfront-terraform-mul-env"
+}
+
+variable "bucket_name" {
+    type = string
+    default = "cloudfront-terraform-multiple-env"
+}
